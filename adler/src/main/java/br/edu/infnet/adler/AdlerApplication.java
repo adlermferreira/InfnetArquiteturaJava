d276340925c7d99d3acc603247e61c71b4a00d06
unknown <adlermf07@gmail.com>
2024-10-30 19:06:24 -0300

    inicialização do projeto

---
+++ adler/src/main/java/br/edu/infnet/adler/AdlerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.adler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdlerApplication.class, args);
+	}
+
+}
---
+++ adler/src/main/java/br/edu/infnet/adler/model/domain/Filme.java
@@ -0,0 +1,5 @@
+package br.edu.infnet.adler.model.domain;
+
+public class Filme {
+
+}
